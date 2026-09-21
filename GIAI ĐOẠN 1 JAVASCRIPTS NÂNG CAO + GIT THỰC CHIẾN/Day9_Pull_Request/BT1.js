3d4696245dfdf657f8c3433e5c02efa70fcf024b
ngoia6664-tech <ngoia6664@gmail.com>
2026-09-21 18:05:31 +0700

    Đây là nhánh bt4

--- GIAI ĐOẠN 1 JAVASCRIPTS NÂNG CAO + GIT THỰC CHIẾN/Day9_Pull_Request/BT1.js
+++ GIAI ĐOẠN 1 JAVASCRIPTS NÂNG CAO + GIT THỰC CHIẾN/Day9_Pull_Request/BT1.js
@@ -1,10 +1,9 @@
-function Nhapgia(Gia){
-    if(typeof Gia==="number"){
-        return Gia;
-    }
-    else{
-        return "Gia phải là số";
-    }
+function Nhapgia(Gia) {
+  if (typeof Gia === "number") {
+    return Gia;
+  } else {
+    return "Gia phải là số";
+  }
 }
 console.log(Nhapgia(2314));
-
+const SANPHAM = { Ten: "LAPTOP", Gia: 5000 }; //Đây là dòng ở nhánh BT_4
